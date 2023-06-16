1d375a65a9770f092176c4623f28631aa76a4dcd
Md Shahidur Rahaman <mdshahidur_rahaman1@baylor.edu>
2023-06-16 01:01:14 -0500

    Update: Path tracking Inclusion with Error

--- src/main/java/com/example/instrumentation/LoggingAgent.java
+++ src/main/java/com/example/instrumentation/LoggingAgent.java
@@ -1,12 +1,21 @@
 package com.example.instrumentation;
 
 import javassist.*;
+import javassist.bytecode.*;
+import org.slf4j.Logger;
 
+import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Modifier;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoggingAgent {
+
+    //static Logger logger = org.slf4j.LoggerFactory.getLogger(LoggingAgent.class);
     public static void premain(String agentArgs, Instrumentation inst) {
         ClassPool pool = ClassPool.getDefault();
         ClassFileTransformer transformer = new ClassFileTransformer() {
@@ -15,11 +24,14 @@ public class LoggingAgent {
                                     ProtectionDomain protectionDomain, byte[] classfileBuffer) {
                 if (className.startsWith("com/example/testlogger")) {
                     try {
-                        CtClass ctClass = pool.makeClass(new java.io.ByteArrayInputStream(classfileBuffer));
+                        CtClass ctClass = pool.makeClass(new ByteArrayInputStream(classfileBuffer));
                         CtMethod[] methods = ctClass.getDeclaredMethods();
 
                         for (CtMethod method : methods) {
-                            addLogStatement(method, ctClass);
+                            addLogStatement(method, ctClass);  /// adding logs
+
+                            // Analyze control-flow graph paths
+                            analyzeControlFlowPaths(method);
                         }
 
                         return ctClass.toBytecode();
@@ -40,5 +52,87 @@ public class LoggingAgent {
             String logStatement = String.format("org.slf4j.LoggerFactory.getLogger(%s.class).info(\"Entering method: %s\");", declaringClass.getName(), methodName);
             method.insertBefore(logStatement);
         }
+    }
+
+    private static void analyzeControlFlowPaths(CtMethod method) {
+        System.out.println("in the flow path method");
+
+        try {
+            MethodInfo methodInfo = method.getMethodInfo();
+            CodeAttribute codeAttribute = methodInfo.getCodeAttribute();
+
+            if (codeAttribute == null) {
+                return; // Skip methods without code (e.g., abstract, native)
+            }
+
+            CodeIterator codeIterator = codeAttribute.iterator();
+            List<Integer> branchPositions = new ArrayList<>();
+            System.out.println("Length of codeIterator " + codeIterator.getCodeLength());
+            while (codeIterator.hasNext()) {
+                System.out.println("i am in loop");
+                int position = codeIterator.next();
+                int opcode = codeIterator.byteAt(position) & 0xFF;
+
+                if (Mnemonic.OPCODE[opcode].startsWith("if")) {
+                    branchPositions.add(position);
+                } else if (Mnemonic.OPCODE[opcode].startsWith("invoke")) {
+                    int index = codeIterator.u16bitAt(position + 1);
+                    String methodRefType = methodInfo.getConstPool().getMethodrefType(index);
+                    CtClass[] parameterTypes = Descriptor.getParameterTypes(methodRefType, method.getDeclaringClass().getClassPool());
+                    CtClass returnType = Descriptor.getReturnType(methodRefType, method.getDeclaringClass().getClassPool());
+
+                    // Skip methods with void return type
+                    if (returnType.equals(CtClass.voidType)) {
+                        continue;
+                    }
+
+                    CtMethod[] declaredMethods = method.getDeclaringClass().getDeclaredMethods();
+                    for (CtMethod declaredMethod : declaredMethods) {
+                        if (declaredMethod.getName().equals(methodInfo.getConstPool().getMethodrefName(index))
+                                && Arrays.equals(declaredMethod.getParameterTypes(), parameterTypes)) {
+                            analyzeControlFlowPaths(declaredMethod);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Perform depth-first traversal to explore all paths
+            traverseControlFlowPaths(branchPositions, 0, new boolean[branchPositions.size() + 1]);
+
+            // Update the modified code
+            method.getMethodInfo().rebuildStackMapIf6(method.getDeclaringClass().getClassPool(), method.getDeclaringClass().getClassFile());
+            method.getDeclaringClass().getClassFile().compact();
+            System.out.println("Finished try block");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void traverseControlFlowPaths(List<Integer> branchPositions, int currentIndex, boolean[] path) {
+        if (currentIndex == branchPositions.size()) {
+            printControlFlowPath(path);
+            return;
+        }
+
+        // Traverse the 'true' branch
+        path[currentIndex] = true;
+        traverseControlFlowPaths(branchPositions, currentIndex + 1, path);
+
+        // Traverse the 'false' branch
+        path[currentIndex] = false;
+        traverseControlFlowPaths(branchPositions, currentIndex + 1, path);
+    }
+
+    private static void printControlFlowPath(boolean[] path) {
+        StringBuilder sb = new StringBuilder();
+
+        for (boolean branch : path) {
+            sb.append(branch ? "true -> " : "false -> ");
+        }
+
+        sb.append("end");
+        String logMessage = "Control-flow path: " + sb.toString();
+        org.slf4j.LoggerFactory.getLogger(LoggingAgent.class).info(logMessage);
     }
 }
